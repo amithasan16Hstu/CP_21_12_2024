9153bd60b8bc3cb48c761716ce25b027ea15fde7
Amit Hasan Sikder <amithasan16hstu@gmail.com>
2024-12-22 14:14:44 +0600

    Add files via upload

---
+++ vec1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   cin>>n;
+   vector<int>vec;
+   for(int i=0;i<n;i++)
+   {
+      int x;cin>>x;
+      vec.push_back(x);
+   }
+   reverse(vec.begin(),vec.end());
+   for(int i=0;i<n;i++)
+   {
+      cout<<vec[i]<<endl;
+   }
+
+}
